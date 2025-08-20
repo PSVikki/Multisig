e2f9d89652881ddf5082ae8a5ee89e5e8073ddda
Panner S Vignesh <110909012+PSVikki@users.noreply.github.com>
2025-08-20 13:13:08 +0530

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -4076,7 +4076,7 @@ msgstr ""
 #: electrum/gui/qml/components/ChannelDetails.qml:426
 #, qt-format
 msgctxt "ChannelDetails|"
-msgid "Channel Backup for %8"
+msgid "Channel Backup for %12"
 msgstr ""
 
 #: electrum/gui/qml/components/ChannelDetails.qml:438
